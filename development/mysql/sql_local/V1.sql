c0f8b59c5bd02db2ff24775797e27bdf3eba9330
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-16 08:08:31 +0900

    update

--- development/mysql/sql_local/V1.sql
+++ development/mysql/sql_local/V1.sql
@@ -1,6 +1,1 @@
-load data local infile '/docker-entrypoint-initdb.d/data/user.csv' into table user fields terminated by ',' lines terminated by '\n' IGNORE 1 LINES;
-load data local infile '/docker-entrypoint-initdb.d/data/session.csv' into table session fields terminated by ',' lines terminated by '\n' IGNORE 1 LINES;
-load data local infile '/docker-entrypoint-initdb.d/data/category.csv' into table category fields terminated by ',' lines terminated by '\n'IGNORE 1 LINES;
-load data local infile '/docker-entrypoint-initdb.d/data/group.csv' into table group_info fields terminated by ',' lines terminated by '\n'IGNORE 1 LINES;
-load data local infile '/docker-entrypoint-initdb.d/data/category_group.csv' into table category_group fields terminated by ',' lines terminated by '\n'IGNORE 1 LINES;
-load data local infile '/docker-entrypoint-initdb.d/data/group_member.csv' into table group_member fields terminated by ',' lines terminated by '\n'IGNORE 1 LINES;
+create index index_value on session(value);
