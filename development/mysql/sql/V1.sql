e8c6fe0ced6caaaaa2268c22a3c715455821604b
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-16 15:35:26 +0900

    add index

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -8,3 +8,6 @@ create index idx2 on record_comment(linked_record_id, created_at DESC);
 -- record_item_file
 create index idx1 on record_item_file(linked_record_id, created_at ASC);
 
+-- record
+create index idx1 on record(status, updated_at desc, record_id asc);
+create index idx2 on record(created_by, status, updated_at desc, record_id asc);
