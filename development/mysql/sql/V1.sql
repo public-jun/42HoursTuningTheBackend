bd816f017c7d0eb6e2605ec5a9da5820813e988c
osorosia <64416398+osorosia@users.noreply.github.com>
2022-04-16 11:09:48 +0900

    index追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,5 +1,10 @@
 -- session
 create index index_value on session(value);
 
--- record comment
-create index idx1 on record_comment(linked_record_id, created_at DESC);
+-- record_comment
+create index idx1 on record_comment(linked_record_id);
+create index idx2 on record_comment(linked_record_id, created_at DESC);
+
+-- record_item_file
+create index idx1 on record_item_file(linked_record_id, created_at ASC);
+
